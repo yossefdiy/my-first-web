d3eaa83fb1f7f6541364defe64fe1c79ba941784
diy <yossefdiy@gmail.com>
2022-06-01 17:58:27 +0300

    dsd

--- src/navbar.js
+++ src/navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes ,Route, BrowserRouter, Link} from "react-router-dom";
+import { BrowserRouter as  Routes ,Route} from "react-router-dom";
 import Player from './player'
 
 
