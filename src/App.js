1c3c9cf47969416ef05faacdd7e460f251821668
diy <yossefdiy@gmail.com>
2022-06-01 19:46:10 +0300

    sdf

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './navbar';
 import Player from './player';
-import { BrowserRouter , Routes ,Route, Link, Router} from "react-router-dom";
+import { BrowserRouter , Routes ,Route, Link} from "react-router-dom";
 
 
 
