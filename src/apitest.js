df967629993d10548c547d2f21d87a5b994d753c
diy <yossefdiy@gmail.com>
2022-07-11 17:43:42 +0300

    search cities

---
+++ src/apitest.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import React,{useState} from "react";
+import Navbar from "./navbar";
+import Weather from "./weather";
+
+
+
+function Api(searchCtiy){
+const [data,setData] = useState({})
+
+     
+
+const searchCtiy1=()=>{
+    const url1='https://raw.githubusercontent.com/royts/israel-cities/master/israel-cities.json'
+//  const url1='https://raw.githubusercontent.com/russ666/all-countries-and-cities-json/6ee538beca8914133259b401ba47a550313e8984/countries.min.json'
+//   const url1='https://datahub.io/core/world-cities/r/world-cities.json'
+    
+        axios.get(url1).then((response)=>{
+            setData(response.data)
+            console.log(response.data)
+     })
+    }
+    
+const [filter,setFilter]= useState([])
+function HandelFilter (event){
+    const Word = event.target.value 
+    const newWord = Object.values (data).filter((value)=>{
+        return value.name.includes(Word)
+    })
+    setFilter(newWord)
+}
+   
+        return(
+           <div className="weather">
+        
+            <input className="input"
+            type='text'    
+          placeholder='city'
+            onChange={HandelFilter}
+            onKeyPress={searchCtiy1} 
+                />
+        <div className="search">
+            {Object.values (filter).map((value,key)=>{
+            return <a className="list">{JSON.stringify(value.name)}</a> 
+          })}
+           {HandelFilter}
+           </div>
+            <div/>
+        
+                    
+          
+      
+      
+<Weather/>
+            </div>
+        
+        )}
+            
+        
+
+    
+export default Api
--- src/weather.js
+++ src/weather.js
@@ -1,41 +1,50 @@
 import axios from "axios";
 import React,{useState} from "react";
 
+
+
 function Weather(){
 const [data,setData] = useState({})
 const [city,setCity] = useState('')
-// const url=`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=8fbf66d40976db983740788788eabe97`
 const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=8fbf66d40976db983740788788eabe97`
 const searchCtiy=(event)=>{
     if (event.key ==='Enter'){
         axios.get(url).then((response)=>{
             setData(response.data)
             console.log(response.data)
+
      })
     }
 }
-    
+
+
+
+   
     
         return(
            <div className="weather">
 
             <div>
-
+  
+             
             <input className="input"
             type='text'
             value={city}
             placeholder='enter city'
             onChange={event=>setCity(event.target.value)}
+
             onKeyPress={searchCtiy}
+           
             />
+         <div className="data">
          <p>מדינה</p>
-         {data.sys ?<p>{data.sys.country}</p>:null}
+         {data.sys ?<p>{data.sys.country}  </p>:null}
          <p>עיר</p>
          {data ?<p>{data.name}</p>:null}
          <p>טמפרטורה</p>
-         {data.main ?<p> <p>°C</p> {data.main.temp }</p>:null}
+         {data.main ?<p> <p>°C</p> {Math.round(data.main.temp)}</p>:null}
 
-
+         </div>
       
 
             </div>
